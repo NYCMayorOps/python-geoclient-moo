6d8bd5e3851f43b2858b7a508cec368e84b21d05
Steve Scott <sscott1@cityops.nyc.gov>
2025-10-29 12:57:10 -0400

    vibe coding baseline not checked yet. passing AI generated tests

--- src/geoclient_moo/models.py
+++ src/geoclient_moo/models.py
@@ -66,34 +66,40 @@ class AddressResponse(BaseResponse):
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "AddressResponse":
         """Create AddressResponse from API response dictionary."""
+        # Handle nested response structure - data might be under "address" key
+        if "address" in data:
+            addr_data = data["address"]
+        else:
+            addr_data = data
+            
         geosupport_info = GeosupportInfo(
-            return_code=data.get("geosupportReturnCode"),
-            reason_code=data.get("reasonCode"),
-            message=data.get("message"),
-            return_code_1a=data.get("geosupportReturnCode2"),
-            reason_code_1a=data.get("reasonCode2"),
-            message_1a=data.get("message2"),
-            return_code_1e=data.get("returnCode1e"),
-            reason_code_1e=data.get("reasonCode1e"),
+            return_code=addr_data.get("geosupportReturnCode"),
+            reason_code=addr_data.get("reasonCode"),
+            message=addr_data.get("message"),
+            return_code_1a=addr_data.get("geosupportReturnCode2"),
+            reason_code_1a=addr_data.get("reasonCode2"),
+            message_1a=addr_data.get("message2"),
+            return_code_1e=addr_data.get("returnCode1e"),
+            reason_code_1e=addr_data.get("reasonCode1e"),
         )
         
         return cls(
             raw_data=data,
-            house_number=data.get("houseNumber"),
-            street_name=data.get("streetName"),
-            borough_code=data.get("boroughCode1In"),
-            borough_name=data.get("boroughName"),
-            zip_code=data.get("zipCode"),
-            latitude=_safe_float(data.get("latitude")),
-            longitude=_safe_float(data.get("longitude")),
-            x_coordinate=_safe_float(data.get("xCoordinate")),
-            y_coordinate=_safe_float(data.get("yCoordinate")),
-            bbl=data.get("bbl"),
-            bin=data.get("buildingIdentificationNumber"),
-            building_identification_number=data.get("buildingIdentificationNumber"),
-            community_district=data.get("communityDistrict"),
-            census_tract=data.get("censusTract2010"),
-            neighborhood_tabulation_area=data.get("nta"),
+            house_number=addr_data.get("houseNumber"),
+            street_name=addr_data.get("boePreferredStreetName"),
+            borough_code=addr_data.get("boroughCode1In"),
+            borough_name=addr_data.get("firstBoroughName"),
+            zip_code=addr_data.get("zipCode"),
+            latitude=_safe_float(addr_data.get("latitude")),
+            longitude=_safe_float(addr_data.get("longitude")),
+            x_coordinate=_safe_float(addr_data.get("xCoordinate")),
+            y_coordinate=_safe_float(addr_data.get("yCoordinate")),
+            bbl=addr_data.get("bbl"),
+            bin=addr_data.get("buildingIdentificationNumber"),
+            building_identification_number=addr_data.get("buildingIdentificationNumber"),
+            community_district=addr_data.get("communityDistrict"),
+            census_tract=addr_data.get("censusTract2010"),
+            neighborhood_tabulation_area=addr_data.get("nta"),
             geosupport=geosupport_info,
         )
 
@@ -103,6 +109,7 @@ class BBLResponse(BaseResponse):
     """Response model for BBL (Borough-Block-Lot) requests."""
     
     borough_code: Optional[str] = None
+    borough_name: Optional[str] = None
     tax_block: Optional[str] = None
     tax_lot: Optional[str] = None
     bbl: Optional[str] = None
@@ -127,26 +134,33 @@ class BBLResponse(BaseResponse):
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "BBLResponse":
         """Create BBLResponse from API response dictionary."""
+        # Handle nested response structure - data is under 'bbl' key
+        if "bbl" in data and isinstance(data["bbl"], dict):
+            bbl_data = data["bbl"]
+        else:
+            bbl_data = data
+            
         geosupport_info = GeosupportInfo(
-            return_code=data.get("geosupportReturnCode"),
-            reason_code=data.get("reasonCode"),
-            message=data.get("message"),
+            return_code=bbl_data.get("geosupportReturnCode"),
+            reason_code=bbl_data.get("reasonCode"),
+            message=bbl_data.get("message"),
         )
         
         return cls(
             raw_data=data,
-            borough_code=data.get("boroughCode"),
-            tax_block=data.get("taxBlock"),
-            tax_lot=data.get("taxLot"),
-            bbl=data.get("bbl"),
-            building_identification_number=data.get("buildingIdentificationNumber"),
-            num_buildings_on_lot=_safe_int(data.get("numBuildingsOnLot")),
-            house_number=data.get("houseNumber"),
-            street_name=data.get("streetName"),
-            latitude=_safe_float(data.get("latitude")),
-            longitude=_safe_float(data.get("longitude")),
-            x_coordinate=_safe_float(data.get("xCoordinate")),
-            y_coordinate=_safe_float(data.get("yCoordinate")),
+            borough_code=bbl_data.get("bblBoroughCode"),
+            borough_name=bbl_data.get("firstBoroughName"),
+            tax_block=bbl_data.get("bblTaxBlock"),
+            tax_lot=bbl_data.get("bblTaxLot"),
+            bbl=bbl_data.get("bbl"),
+            building_identification_number=bbl_data.get("buildingIdentificationNumber"),
+            num_buildings_on_lot=_safe_int(bbl_data.get("numberOfExistingStructuresOnLot")),
+            house_number=bbl_data.get("giLowHouseNumber1"),
+            street_name=bbl_data.get("giStreetName1"),
+            latitude=_safe_float(bbl_data.get("latitude")),
+            longitude=_safe_float(bbl_data.get("longitude")),
+            x_coordinate=_safe_float(bbl_data.get("internalLabelXCoordinate")),
+            y_coordinate=_safe_float(bbl_data.get("internalLabelYCoordinate")),
             geosupport=geosupport_info,
         )
 
@@ -176,24 +190,30 @@ class BINResponse(BaseResponse):
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "BINResponse":
         """Create BINResponse from API response dictionary."""
+        # Handle nested response structure - data is under 'bin' key
+        if "bin" in data and isinstance(data["bin"], dict):
+            bin_data = data["bin"]
+        else:
+            bin_data = data
+            
         geosupport_info = GeosupportInfo(
-            return_code=data.get("geosupportReturnCode"),
-            reason_code=data.get("reasonCode"),
-            message=data.get("message"),
+            return_code=bin_data.get("geosupportReturnCode"),
+            reason_code=bin_data.get("reasonCode"),
+            message=bin_data.get("message"),
         )
         
         return cls(
             raw_data=data,
-            building_identification_number=data.get("buildingIdentificationNumber"),
-            bbl=data.get("bbl"),
-            house_number=data.get("houseNumber"),
-            street_name=data.get("streetName"),
-            borough_code=data.get("boroughCode"),
-            borough_name=data.get("boroughName"),
-            latitude=_safe_float(data.get("latitude")),
-            longitude=_safe_float(data.get("longitude")),
-            x_coordinate=_safe_float(data.get("xCoordinate")),
-            y_coordinate=_safe_float(data.get("yCoordinate")),
+            building_identification_number=bin_data.get("buildingIdentificationNumber"),
+            bbl=bin_data.get("bbl"),
+            house_number=bin_data.get("giLowHouseNumber1"),
+            street_name=bin_data.get("giStreetName1"),
+            borough_code=bin_data.get("bblBoroughCode"),
+            borough_name=bin_data.get("firstBoroughName"),
+            latitude=_safe_float(bin_data.get("latitude")),
+            longitude=_safe_float(bin_data.get("longitude")),
+            x_coordinate=_safe_float(bin_data.get("internalLabelXCoordinate")),
+            y_coordinate=_safe_float(bin_data.get("internalLabelYCoordinate")),
             geosupport=geosupport_info,
         )
 
